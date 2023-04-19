34c55547f9dcb14936d4f43b144abb718af258e5
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-04-20 08:44:15 +0900

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "foo" {
-  source = "github.com/suzuki-shunsuke/test-terraform-module-renovate//modules/foo?ref=main"
+  source = "github.com/suzuki-shunsuke/test-terraform-module-renovate//modules/foo?ref=module_modules_foo_v0.1.0"
 }
 
 module "bar" {
